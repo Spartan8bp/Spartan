c2b3f3725114569e5bdf1d2324ea9c55b358dbe8
Spartan8bp <samuel.bras.8bp@gmail.com>
2025-08-11 20:06:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def agendador():
         hora = agora.strftime('%H:%M')
         if hora == "07:00":
             enviar_motivacional()
-        if hora == "19:55" or hora == "20:00":
+        if hora == "20:09" or hora == "20:10":
             parabens_aniversariantes()
         if hora == "11:00":
             parabens_do_mes()
